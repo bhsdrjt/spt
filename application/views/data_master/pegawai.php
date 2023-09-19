149e8659fcaa7ae8a749bb95e77a8101d742b286
bayu <bayuhadisudrajat@gmail.com>
2023-09-20 00:00:41 +0800

    perbaikanlagi

--- application/views/data_master/pegawai.php
+++ application/views/data_master/pegawai.php
@@ -139,7 +139,6 @@
               <select class="form-select" name="statusPegawai" id="statusPegawai">
               <option value="Pegawai BKSDA">Pegawai BKSDA</option>
                 <option value="Non Pegawai BKSDA">Non Pegawai BKSDA</option>
-                <option value="Non Pegawai BKSDA">Non Pegawai BKSDA</option>
                 <option value="Pensiun">Pensiun</option>
                 <option value="Keluar">Keluar</option>
                 <option value="Meninggal Dunia">Meninggal Dunia</option>
@@ -216,7 +215,6 @@
               <label for="statusPegawai"class="form-label">Status Pegawai</label>
               <select class="form-select statusPegawai" name="statusPegawai"id="statusPegawai2">
                 <option value="Pegawai BKSDA">Pegawai BKSDA</option>
-                <option value="Non Pegawai BKSDA">Non Pegawai BKSDA</option>
                 <option value="Non Pegawai BKSDA">Non Pegawai BKSDA</option>
                 <option value="Pensiun">Pensiun</option>
                 <option value="Keluar">Keluar</option>
